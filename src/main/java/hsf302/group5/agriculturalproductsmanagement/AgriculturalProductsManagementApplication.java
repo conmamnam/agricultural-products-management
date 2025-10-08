cdaf7e6073a862d164684dd20c41c813a93aad78
NamNguyen <conmamnam@gmail.com>
2025-10-08 21:54:26 +0700

    first commit

---
+++ src/main/java/hsf302/group5/agriculturalproductsmanagement/AgriculturalProductsManagementApplication.java
@@ -0,0 +1,13 @@
+package hsf302.group5.agriculturalproductsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriculturalProductsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgriculturalProductsManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hsf302/group5/agriculturalproductsmanagement/AgriculturalProductsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package hsf302.group5.agriculturalproductsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgriculturalProductsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
